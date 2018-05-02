641777fc565fb75d162fd0d3a6b5a96406776097
杨帆 <incomparable9527@foxmail.com>
2018-05-02 13:57:28 +0800

    hash

--- src/root.component.js
+++ src/root.component.js
@@ -3,13 +3,32 @@ import { Provider } from 'react-redux'
 import App from './models/example/App'
 import BasicLayout from './layouts/BasicLayout'
 import UserLayout from './layouts/UserLayout'
-import { BrowserRouter, Route, hashHistory, Switch, Redirect } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Route,
+  HashRouter,
+  hashHistory,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 import { getRouterData } from './common/router'
+import createHistory from 'history/createBrowserHistory'
+const history = createHistory()
+const location = history.location
+history.listen(function (location, action) {
+  // youAreHere.textContent = location.pathname
+  console.log("网址改变了,我这里已经知道")
+  console.log(location, action)
+  
+})
 export default class RootComponent extends React.Component {
     state = { store: this.props.store, globalEventDistributor: this.props.globalEventDistributor };
 
     componentDidCatch(error, info) {
       console.log(error, info)
+    }
+    componentWillMount(){
+
     }
 
     setStore(store) {
@@ -28,11 +47,13 @@ export default class RootComponent extends React.Component {
       console.log(this.props)
       if (this.state.store && this.state.globalEventDistributor) {
         ret = <Provider store={this.state.store}>
-          <BrowserRouter>
+          < HashRouter history = {
+            history
+          } >
             <Switch>
               <Route  render={props => <BasicLayout {...customProps} {...props} />} />
             </Switch>          
-          </BrowserRouter>
+          </HashRouter>
 
         </Provider>
       }
