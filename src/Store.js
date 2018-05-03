2ad955da71f617d849e1dcbba52e61df6734f080
杨帆 <incomparable9527@foxmail.com>
2018-05-03 13:40:27 +0800

    menudate

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
 const initialState = {
   namespace: 'base', // 全局Store的命名空间,每个项目必须不一样.请记得修改
@@ -6,18 +6,60 @@ const initialState = {
   refresh: 0
 }
 
+
+let menuDate = {
+  name: '列表页',
+  icon: 'table',
+  path: 'list',
+  children: [
+    {
+      name: '查询表格',
+      path: 'table-list',
+    },
+    {
+      name: '标准列表',
+      path: 'basic-list',
+    },
+    {
+      name: '卡片列表',
+      path: 'card-list',
+    },
+    {
+      name: '搜索列表',
+      path: 'search',
+      children: [
+        {
+          name: '搜索列表（文章）',
+          path: 'articles',
+        },
+        {
+          name: '搜索列表（项目）',
+          path: 'projects',
+        },
+        {
+          name: '搜索列表（应用）',
+          path: 'applications',
+        },
+      ],
+    },
+  ],
+}
+
 function reducer(state = initialState, action) {
   switch (action.type) {
     case 'INCREMENT':
-      return { ...state,
+      return {
+        ...state,
         count: state.count + 1
       }
     case 'DECREMENT':
-      return { ...state,
+      return {
+        ...state,
         count: state.count - 1
       }
     case 'REFRESH':
-      return { ...state,
+      return {
+        ...state,
         refresh: state.refresh + 1
       }
     default:
@@ -25,4 +67,8 @@ function reducer(state = initialState, action) {
   }
 }
 
+function menu(){
+  return menuDate
+}
+combineReducers(menu, reducer)
 export const storeInstance = createStore(reducer)
