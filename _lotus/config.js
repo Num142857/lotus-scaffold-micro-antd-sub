a65702646a284812ec26f5f561c5cf60c3ee61f6
杨帆 <incomparable9527@foxmail.com>
2018-05-04 13:56:46 +0800

    脚手架打包

---
+++ _lotus/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'files': [
+    'config/',
+    'public/',
+    'scripts/',
+    'src/',
+    'package.json',
+    '.eslintrc.js',
+    'jsconfig.json',
+    'tsconfig.json',
+    'README.md'
+  ]
+}
